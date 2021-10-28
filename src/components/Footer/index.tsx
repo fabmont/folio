15168d369bb471962350f851d5e57f68bd04ff7b
fabmont <fabriciosantos47@gmail.com>
2021-10-28 12:49:12 -0300

    adjusts the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,18 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 
 const Footer: React.FC = () => (
-  <Box align="center" opacity={0.4} fontSize="sm">
-    &copy; {new Date().getFullYear()} Fabrício Monteiro. All Rights Reserved.
+  <Box
+    display="flex"
+    alignItems="center"
+    textAlign="center"
+    opacity={0.4}
+    fontSize="sm"
+    w="100%"
+    py="4"
+  >
+    <Text w="100%">
+      &copy; {new Date().getFullYear()} Fabrício Monteiro. All Rights Reserved.
+    </Text>
   </Box>
 );
 
