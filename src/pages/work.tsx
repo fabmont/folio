8e15cb9c6170d1c9433a8dc59feb42fa4abe5a72
Fabrício Monteiro <fabriciosantos47@gmail.com>
2022-11-11 12:32:34 -0300

    fixes the careers heading

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -11,7 +11,9 @@ const Work: React.FC = () => (
     </Head>
 
     <Section delay="0.2">
-      <Heading variant="section-title">Career</Heading>
+      <Heading variant="section-title" mb="8" mt="8" fontSize="3xl">
+        Career
+      </Heading>
       {workList.map((item) => (
         <Box my="8" key={item.title}>
           <Text opacity={0.6} fontSize="lg">
